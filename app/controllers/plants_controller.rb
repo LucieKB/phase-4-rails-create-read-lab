c0302efcff0011a6a253fb55d935c86345ee0922
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-08-20 11:53:05 -0600

    commit tree 558e42460a7b363292e6b740901729110982fb8b

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,21 @@
+class PlantsController < ApplicationController
+
+    def index
+        render json: Plant.all
+    end
+
+    def show 
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: {error: "The plant you are looking for doesn't exist"}, status: :not_found
+        end
+    end
+
+    def create 
+        plant = Plant.create(name: params[:name], image: params[:image], price: params[:price])
+        render json: plant, status: :created
+    end
+
+end
